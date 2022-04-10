5a689daccc4112b53b416761080308568dbb4185
tommyjqliu <hentaitommy@qq.com>
2022-04-10 20:25:15 +0800

    test localStorage

--- sit_time_counter_widget/index.jsx
+++ sit_time_counter_widget/index.jsx
@@ -1,10 +1,13 @@
 'use strict';
-const { useState } = React
+const { useState, useEffect } = React
 
 
 function Main() {
-  const [count, setCount] = useState(0)
-
+  const localCount = parseInt(localStorage.getItem('myCount'));
+  const [count, setCount] = useState(localCount ? localCount : 0)
+  useEffect(() => {
+    localStorage.setItem('myCount', count);
+  })
   return (
     <div className="bg-slate-300 h-screen rounded-3xl" onClick={() => setCount(count + 1)}>
       {count}
--- sit_time_counter_widget/static/index.js
+++ sit_time_counter_widget/static/index.js
@@ -3,15 +3,21 @@
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 var _React = React,
-    useState = _React.useState;
+    useState = _React.useState,
+    useEffect = _React.useEffect;
 
 
 function Main() {
-  var _useState = useState(0),
+  var localCount = parseInt(localStorage.getItem('myCount'));
+
+  var _useState = useState(localCount ? localCount : 0),
       _useState2 = _slicedToArray(_useState, 2),
       count = _useState2[0],
       setCount = _useState2[1];
 
+  useEffect(function () {
+    localStorage.setItem('myCount', count);
+  });
   return React.createElement(
     'div',
     { className: 'bg-slate-300 h-screen rounded-3xl', onClick: function onClick() {
