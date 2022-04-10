d58b9be945c8925eb1d8cc691b4f1116c28c48c5
tommyjqliu <hentaitommy@qq.com>
2022-04-10 21:09:06 +0800

    compare

--- sit_time_counter_widget/service-worker.js
+++ sit_time_counter_widget/service-worker.js
@@ -1,19 +1,19 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-				'./',
-        './static/index.js',
-        './static/index.css',
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open('v1').then(function(cache) {
+//       return cache.addAll([
+// 				'./',
+//         './static/index.js',
+//         './static/index.css',
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
